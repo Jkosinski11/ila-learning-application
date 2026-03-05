99fcca505083aa58a10e1523eb89414f38387335
Jkosinski11 <kosinski.jacob11@gmail.com>
2026-03-05 13:28:30 -0500

    Databse working plus new docker file

--- my-react-app/backend/db_init.sql
+++ my-react-app/backend/db_init.sql
@@ -2,14 +2,15 @@ BEGIN;
 
 
 -- using the UID created by firbase at login
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS  users (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   firebase_uid TEXT UNIQUE NOT NULL,
   email TEXT UNIQUE,
+  account_type TEXT,
   created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE portfolios (
+CREATE TABLE IF NOT EXISTS  portfolios (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   name TEXT NOT NULL DEFAULT 'Main',
@@ -17,7 +18,7 @@ CREATE TABLE portfolios (
   created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE user_stocks (
+CREATE TABLE IF NOT EXISTS  user_stocks (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   stock_tic TEXT,
